6ebfdccae7fe4af7fd9c4ea8db5ba2b68ed74562
Rayhanendra <mrayhann@gmail.com>
2023-06-15 13:49:02 +0700

    build: unit testing preparation. add vitest, react testing library, and configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:vitest/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'vitest'],
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
