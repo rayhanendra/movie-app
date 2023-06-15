8b68e3d22ae9cab6d01163c278c0b8315f5b1b9d
Rayhanendra <mrayhann@gmail.com>
2023-06-16 01:38:07 +0700

    feat: add scroll to top on every route change

--- src/templates/MainLayout.tsx
+++ src/templates/MainLayout.tsx
@@ -1,11 +1,13 @@
 import NavigationBar from '@/components/organisms/NavigationBar';
 import SideBar from '@/components/organisms/SideBar';
+import ScrollToTop from '@/helpers/ScrollToTop';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout: React.FC = () => {
   return (
     <>
+      <ScrollToTop />
       <div className="tw-h-screen md:tw-container md:tw-mx-auto md:tw-grid md:tw-grid-cols-12 md:tw-space-x-2">
         <div className="tw-hidden md:tw-col-span-2 md:tw-block">
           <SideBar />
